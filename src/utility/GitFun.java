ebacab72a477306268b03f5ee85d867bcef4ce9a
JHY34 <hamityilmaz34@gmail.com>
2020-08-05 20:04:30 -0500

    updated GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,5 +3,7 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Git is Fun!");
+        // This is the update part before sending to GitHub
+        System.out.println("Let see what will happen.");
     }
 }
